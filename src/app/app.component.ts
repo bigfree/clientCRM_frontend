d516deaf5368a71c4a23b3fb598d123b42939989
Adam Mikó <okukster@gmail.com>
2019-03-04 22:31:10 +0100

    popici veci

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,67 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Apollo } from 'apollo-angular';
+import gql from 'graphql-tag';
+
+const ClientsQuery = gql`
+	query clients {
+		getClients {
+			id
+			name
+			description
+			emails {
+				id
+				email
+			}
+			created_at
+		}
+	}
+`;
+type Response = {
+	getClients: any;
+}
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+	selector: 'app-root',
+	templateUrl: './app.component.html',
+	styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'frontend';
+export class AppComponent implements OnInit {
+	title = 'frontend';
+
+	public clients: any;
+
+	constructor(private apollo: Apollo) {}
+
+	ngOnInit() {
+
+		this.apollo.watchQuery<Response>({query: ClientsQuery}).valueChanges.subscribe((res) => {
+			console.log(res.data.getClients);
+		});
+
+		// this.data = this.apollo.watchQuery({query: ClientsQuery}).valueChanges.pipe(map((res) => {
+		// 	console.log(res);
+		// }));
+
+		// this.data = this.apollo
+		// 	.watchQuery({query: ClientsQuery})
+		// 	.valueChanges.pipe(map(({data}) => {
+		// 		console.log(data);
+		// 	}));
+
+		// this.apollo.watchQuery({
+		// 	query: gql`
+		// 		{
+		// 			getClients {
+		// 				id
+		// 				name
+		// 				description
+		// 			}
+		// 		}
+		// 	`,
+		// })
+		// .valueChanges.subscribe(res => {
+		// 	console.log(res);
+		// 	this.users = res.data && res.data.getClients;
+		// });
+	}
 }
