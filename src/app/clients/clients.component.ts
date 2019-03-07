76ed812df9b86beb13300caeb2ccf35c55e51d2e
Adam Miko <miko@monumental.sk>
2019-03-07 16:30:37 +0100

    kokutek

--- src/app/clients/clients.component.ts
+++ src/app/clients/clients.component.ts
@@ -1,6 +1,7 @@
+import { QueryRef } from 'apollo-angular';
 import { Component, OnInit } from '@angular/core';
 import { Observable, Subscription } from 'rxjs';
-import { map } from 'rxjs/operators';
+import { map, pluck } from 'rxjs/operators';
 
 import { AllClientsGQL, Client } from '../generated/graphql';
 import { NewClientGQL } from '../generated/graphql';
@@ -12,30 +13,30 @@ import { NewClientGQL } from '../generated/graphql';
 })
 export class ClientsComponent implements OnInit {
 
+	// public clients: Observable<Client[]>;
+	// public clientSubscription: Subscription;
+	// public clientArr: Client[] = [];
+
+	private ClientsQuery: QueryRef<any>;
+
 	public clients: Observable<Client[]>;
-	public clientSubscription: Subscription;
-	public clientArr: Client[] = [];
 
 	constructor(
 		private allClientsGQL: AllClientsGQL,
 		private newClientGQL: NewClientGQL
 		) {
-		newClientGQL.subscribe(({data}) => {
-			this.clientArr = [...this.clientArr, data.clientAdded];
-		});
+		// newClientGQL.subscribe(({data}) => {
+		// 	this.clientArr = [...this.clientArr, data.clientAdded];
+		// });
 	}
 
 	ngOnInit() {
 
-		// this.clientsSubscription = this.apollo.subscribe({
-		// 	query: this.allClientsGQL.client
-		// }).subscribe(({ data }) => {
-		// 	this.clients = [...this.clients, data.clients];
-		// });
+		this.clients = this.allClientsGQL.watch().valueChanges.pipe(pluck("data", "clients"));
 
-		this.clients = this.allClientsGQL.watch().valueChanges.pipe(
-			map(({data}) => data.clients)
-		);
+		// this.clients = this.allClientsGQL.watch().valueChanges.pipe(
+		// 	map(({data}) => data.clients)
+		// );
 	}
 
 }
